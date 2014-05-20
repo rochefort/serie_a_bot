ac27a6fc0aa2b79a657ae86363810f596bd2c920
rochefort <terasawan@gmail.com>
2014-05-21 05:28:11 +0900

    Add test for initizlizing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '../lib')))
+ENV['RUBY_ENV'] = 'test'
+require_relative '../config/boot.rb'
+# require_relative '../lib/model'
+
